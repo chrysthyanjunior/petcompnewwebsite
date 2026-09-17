a6f4209b5bcaae6ef42ee8be13d191fa7d6a8995
chrysthyanjunior <chrysthyan.almeida.jr@gmail.com>
2026-09-17 10:48:06 -0300

    Upadatee footer

--- footer.php
+++ footer.php
@@ -39,7 +39,7 @@
                 </h2>
                 <ul class="ulist">
                     <li class="litem">
-                        <a target="_blank"class="item-button" href="monitoria">Chrysthyan</a>
+                        <a target="_blank"class="item-button" href="monitoria">Chrysthyann</a>
                     </li>
                     <li class="litem">
                         <a target="_blank" class="item-button" href="desenvolvimento">Fáb. Software</a>
